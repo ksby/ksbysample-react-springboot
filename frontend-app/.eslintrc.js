8b8dfa517f02242941358ce80286f23bf943130c
ksby <kusa@db3.so-net.ne.jp>
2021-04-29 08:06:01 +0000

    .eslintrc.js に prettier に必要な設定を追加しました

--- frontend-app/.eslintrc.js
+++ frontend-app/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "plugin:import/typescript",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'prettier',
   ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
